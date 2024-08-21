685dccff2c9e7ae2e1462f64ba5516861bfde705
alexrsilva-rj <alexrsilva@gmail.com>
2024-08-20 22:07:48 -0300

    adicionando o eureka-client

--- payroll-api/src/main/java/br/com/alexrsilva/payrollapi/feignClients/UserFeign.java
+++ payroll-api/src/main/java/br/com/alexrsilva/payrollapi/feignClients/UserFeign.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
-@FeignClient(name="user-api",url="http://localhost:8000")
+@FeignClient(name="user-api",url="http://localhost:8001")
 public interface UserFeign {
     @GetMapping(value = "/api/users/{id}")
     ResponseEntity<User> findById(@PathVariable Long id);
